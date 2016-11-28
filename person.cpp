876d198ee78077c40426793acb031638785d9713
Fanarito <viktor.saevars@gmail.com>
2016-11-28 11:48:35 +0000

    added base person

--- person.cpp
+++ person.cpp
@@ -4,3 +4,32 @@ person::person()
 {
 
 }
+
+std::string getName() {
+    return m_name;
+}
+void setName(std::string name) {
+    m_name = name;
+}
+
+
+std::string getSex() {
+    return m_sex;
+}
+void setSex(std::string sex) {
+    m_sex = sex;
+}
+
+int getBirthYear() {
+    return m_birthyear;
+}
+void setBirthYear(int birthyear) {
+    m_birthyear = birthyear;
+}
+
+int getDeathYear() {
+    return m_deathyear;
+}
+void setDeathYear(int deathyear) {
+    m_deathyear = deathyear;
+}
--- person.h
+++ person.h
@@ -1,11 +1,29 @@
 #ifndef PERSON_H
 #define PERSON_H
 
+#include <string>
 
 class person
 {
+private:
+    std::string m_name;
+    std::string m_sex;
+    int m_birthyear;
+    int m_deathyear;
 public:
     person();
+
+    std::string getName();
+    void setName(std::string name);
+
+    std::string getSex();
+    void setSex(std::string sex);
+
+    int getBirthYear();
+    void setBirthYear(int birthyear);
+
+    int getDeathYear();
+    void setDeathYear(int deathyear);
 };
 
 #endif // PERSON_H
