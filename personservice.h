df99cb6fde8f8268f66943aa01f7ff4639110eb6
harpa96 <harpah16@ru.is>
2016-11-28 14:05:16 +0000

    bjo til foll

--- personservice.h
+++ personservice.h
@@ -13,8 +13,17 @@ public:
     personservice();
     void addPerson(std::string name, std::string gender, int birthyear, int deathyear);
     vector<person> getPersons();
+    vector<person> alphabetical();
+    vector<person> reverseAlphabetical();
+    vector<person> ageDescending();
+    vector<person> ageAscending();;
+    vector<person> deathDescending();
+    vector<person> deathAscending();
+    vector<person> genderMale();
+    vector<person> genderFemale();
 private:
     vector<person> _persons;
+    vector<person> peopleSorted;
 };
 
 #endif // PERSONSERVICE_H
