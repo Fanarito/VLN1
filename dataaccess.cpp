5ea8d091d06590f7489392b1bc441fd1fc467ddb
Fanarito <viktor.saevars@gmail.com>
2016-11-28 13:36:45 +0000

    added read from file

--- dataaccess.cpp
+++ dataaccess.cpp
@@ -4,3 +4,27 @@ dataaccess::dataaccess()
 {
 
 }
+
+std::vector<std::string> split(const std::string &s, char delim) {
+    std::stringstream ss(s);
+    std::string item;
+    std::vector<std::string> tokens;
+    while (std::getline(ss, item, delim)) {
+        tokens.push_back(item);
+    }
+    return tokens;
+}
+
+/*
+ * Reads the file and saves it to a vector
+ */
+void dataaccess::read() {
+    std::fstream file(FILENAME);
+
+    std::vector<std::string> p;
+    std::string line;
+    while(std::getline(file, line)) {
+        p = split(line, DELIMITER);
+        people.push_back(person(p[0], p[1], std::stoi(p[2]), std::stoi(p[3])));
+    }
+}
--- dataaccess.h
+++ dataaccess.h
@@ -1,11 +1,24 @@
 #ifndef DATAACCESS_H
 #define DATAACCESS_H
 
+#include <fstream>
+#include <string>
+#include <vector>
+#include <sstream>
+#include <iostream>
+
+#include "person.h"
+
+static std::string FILENAME = "../VNL1/personsNew.txt";
+static char DELIMITER = '|';
 
 class dataaccess
 {
+private:
+    std::vector<person> people;
 public:
     dataaccess();
+    void read();
 };
 
 #endif // DATAACCESS_H
