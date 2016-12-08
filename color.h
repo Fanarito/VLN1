b4e32f147b54cc115272248ec9e7c22841e3ac38
Fanarito <viktor.saevars@gmail.com>
2016-12-08 14:08:34 +0000

    possible color fix

--- color.h
+++ color.h
@@ -3,6 +3,40 @@
 
 #include <ostream>
 
+#if defined(WIN32) || defined(_WIN32) || defined(__WIN32) && !defined(__CYGWIN__)
+#include <windows.h>
+
+namespace Color {
+    enum Code {
+        FG_RED      = 31,
+        FG_GREEN    = 32,
+        FG_BLUE     = 34,
+        FG_DEFAULT  = 39,
+        BG_RED      = 41,
+        BG_GREEN    = 42,
+        BG_BLUE     = 44,
+        BG_DEFAULT  = 49
+    };
+
+    class Modifier {
+    private:
+        Code code;
+    public:
+        Modifier(Code pCode) : code(pCode) {}
+        friend std::ostream& operator<<(std::ostream &os, const Modifier &mod)
+        {
+            HANDLE  hConsole;
+            hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+
+            SetConsoleTextAttribute(hConsole, mod.code);
+
+            return os << "test";
+        }
+    };
+}
+
+#else
+
 namespace Color {
     enum Code {
         FG_RED      = 31,
@@ -27,4 +61,5 @@ namespace Color {
     };
 }
 
+#endif // not windows
 #endif // COLOR_H
