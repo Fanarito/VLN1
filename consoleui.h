519a8c71164332c547f528d528b6e6a704a83802
Fanarito <viktor.saevars@gmail.com>
2016-12-12 14:16:22 +0000

    added missing files

---
+++ consoleui.h
@@ -0,0 +1,57 @@
+#ifndef CONSOLEUI_H
+#define CONSOLEUI_H
+
+
+#include "service.h"
+#include "utils.h"
+#include "constants.h"
+#include "color.h"
+
+using namespace constants;
+
+class consoleui
+{
+public:
+    consoleui();
+    void run();
+private:
+    service ps;
+    string command;
+    bool EXIT;
+
+    void listMenu(string choice);
+    void addMenu(string choice);
+    void changeMenu(string choice);
+    void removeMenu(string choice);
+    void sortMenu(string choice);
+    int searchMenu(string choice, bool printRes=true);
+    void infoMenu(string choice);
+
+    void tablePrint(string s, int width);
+
+    std::string getValidNationality(std::string message, bool allow_empty=false);
+    std::string getValidComputerType(std::string message, bool allow_empty=false);
+
+    void printDetailsPerson(person p);
+    void printDetailsComputer(computer c);
+
+    void printInfoPerson(person p);
+    void printInfoComputer(computer c);
+
+    void printPersons(vector<person> p);
+    void printPersons(person p);
+
+    void printComputers(vector<computer> c);
+    void printComputers(computer c);
+
+    void printPersonConnections(vector<person> persons);
+    void printComputerConnections(vector<computer> computers);
+
+    std::string getInputString(std::string message, bool multiToken = MULTI, string expected = NO_EXP, bool allow_number = false);
+
+    int getInputInt(std::string message, int low_bound = INT_MIN, int high_bound = INT_MAX, bool allow_empty = false);
+
+    void printChristmas();
+};
+
+#endif // CONSOLEUI_H
